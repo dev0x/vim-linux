ab2e507f8f09f3d07f93fce359ee73c6ef3416f1
dev0x <racine.matt@gmail.com>
2013-10-21 09:51:48 -0500

    pulling powerline and gitgutter from script

--- bundle/goClone.sh
+++ bundle/goClone.sh
@@ -5,7 +5,4 @@ git clone https://github.com/scrooloose/nerdtree.git;
 git clone https://github.com/jistr/vim-nerdtree-tabs.git;
 git clone https://github.com/scrooloose/syntastic.git;
 git clone git://github.com/altercation/vim-colors-solarized.git;
-git clone git://github.com/airblade/vim-gitgutter.git; 
-git clone git://github.com/Lokaltog/powerline.git; 
-git clone git://github.com/stephenmckinney/vim-solarized-powerline.git
 git clone https://github.com/bling/vim-airline.git
